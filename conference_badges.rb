43ad988e11297b4184177429ce6a9dd9db032c07
Dean Erickson <dmepilot@yahoo.com>
2020-06-06 03:23:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect {|badge| "Hello, my name is #{badge}."}
+end
+
+def assign_rooms(names)
+  names.collect do |name|
+  
+  end
+end
